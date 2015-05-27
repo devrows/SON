2e836fbdff9cff5bd8d41831225dc7827f2ed5b3
Devin Rose <drose02@mail.uoguelph.ca>
2015-05-27 12:20:08 -0400

    Improved readability and cooling water map

--- src/creatingMapFiles.jl
+++ src/creatingMapFiles.jl
@@ -15,8 +15,8 @@ writedlm("/Users/devin-rose92/desktop/SONCLWRP/SON/maps/numberedMap_array.csv", 
 habitatType_array = habitatTypeArray(habitat_testArray, fileLocation)
 
 writedlm("/Users/devin-rose92/desktop/SONCLWRP/SON/maps/habitatType_array.csv", habitatType_array,',');
-draw(SVG("/Users/devin-rose92/desktop/SONCLWRP/SON/Images/habitatType.svg", 8inch, 6inch), spy(habitatType_array))
-draw(SVG("/Users/devin-rose92/desktop/SONCLWRP/SON/Images/habitatTypeForCoolingWater_50x50.svg", 8inch, 6inch), spy(habitatType_array [310:360, 310:360]))
+draw(SVG("/Users/devin-rose92/desktop/SONCLWRP/Images/habitatType.svg", 8inch, 6inch), spy(habitatType_array))
+draw(SVG("/Users/devin-rose92/desktop/SONCLWRP/Images/habitatTypeForCoolingWater_50x50.svg", 8inch, 6inch), spy(habitatType_array [310:360, 310:360]))
 
 
 #For spawning area
@@ -31,8 +31,8 @@ for column = 1:size(boolSpawn_array)[2]
   boolSpawn_array [1,column] = true
 end
 
-draw(SVG("/Users/devin-rose92/desktop/SONCLWRP/SON/Images/spawningImage.svg", 8inch, 6inch), spy(boolSpawn_array))
-draw(SVG("/Users/devin-rose92/desktop/SONCLWRP/SON/Images/shorelineWithSpawning.svg", 8inch, 6inch), spy(boolSpawnWithShore_array))
+draw(SVG("/Users/devin-rose92/desktop/SONCLWRP/Images/spawningImage.svg", 8inch, 6inch), spy(boolSpawn_array))
+draw(SVG("/Users/devin-rose92/desktop/SONCLWRP/Images/shorelineWithSpawning.svg", 8inch, 6inch), spy(boolSpawnWithShore_array))
 
 
 #For cooling water intake location
@@ -40,7 +40,7 @@ coolingWaterIntake_array = coolingWaterIntakeLocation(fileLocation, habitat_test
 coolingWaterImage_array = coolingWaterVisualization(fileLocation, habitat_testArray)
 
 writedlm("/Users/devin-rose92/desktop/SONCLWRP/SON/maps/coolingWaterIntakeArray.csv", coolingWaterIntake_array,',');
-draw(SVG("/Users/devin-rose92/desktop/SONCLWRP/SON/Images/coolingWaterIntakeLocation_15x15.svg", 8inch, 6inch), spy(coolingWaterImage_array [330:345, 325:340]))
-draw(SVG("/Users/devin-rose92/desktop/SONCLWRP/SON/Images/coolingWaterIntakeLocation_25x25.svg", 8inch, 6inch), spy(coolingWaterImage_array [325:350, 320:345]))
-draw(SVG("/Users/devin-rose92/desktop/SONCLWRP/SON/Images/coolingWaterIntakeLocation_50x50.svg", 8inch, 6inch), spy(coolingWaterImage_array [310:360, 310:360]))
-draw(SVG("/Users/devin-rose92/desktop/SONCLWRP/SON/Images/coolingWaterIntakeLocation_100x100.svg", 8inch, 6inch), spy(coolingWaterImage_array [286:384, 286:384]))
+draw(SVG("/Users/devin-rose92/desktop/SONCLWRP/Images/coolingWaterIntakeLocation_15x15.svg", 8inch, 6inch), spy(coolingWaterImage_array [330:345, 325:340]))
+draw(SVG("/Users/devin-rose92/desktop/SONCLWRP/Images/coolingWaterIntakeLocation_25x25.svg", 8inch, 6inch), spy(coolingWaterImage_array [325:350, 320:345]))
+draw(SVG("/Users/devin-rose92/desktop/SONCLWRP/Images/coolingWaterIntakeLocation_50x50.svg", 8inch, 6inch), spy(coolingWaterImage_array [310:360, 310:360]))
+draw(SVG("/Users/devin-rose92/desktop/SONCLWRP/Images/coolingWaterIntakeLocation_100x100.svg", 8inch, 6inch), spy(coolingWaterImage_array [286:384, 286:384]))
